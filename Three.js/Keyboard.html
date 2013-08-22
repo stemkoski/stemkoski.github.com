1ce64af185678976d9adc3bc5323b9f42e2adc0d
stemkoski <stemkoski@gmail.com>
2013-08-22 13:53:59 -0400

    New keyboard input manager.

---
+++ Three.js/Keyboard.html
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Keyboard Input (Three.js)</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel=stylesheet href="css/base.css"/>
+</head>
+<body>
+
+<script src="js/Three.js"></script>
+<script src="js/Detector.js"></script>
+<script src="js/Stats.js"></script>
+<script src="js/OrbitControls.js"></script>
+<script src="js/KeyboardState.js"></script>
+<script src="js/THREEx.FullScreen.js"></script>
+<script src="js/THREEx.WindowResize.js"></script>
+
+<!-- Code to display an information button and box when clicked. -->
+<script src="js/jquery-1.9.1.js"></script>
+<script src="js/jquery-ui.js"></script>
+<link rel=stylesheet href="css/jquery-ui.css" />
+<link rel=stylesheet href="css/info.css"/>
+<script src="js/info.js"></script>
+<div id="infoButton"></div>
+<div id="infoBox" title="Demo Information">
+Controls: Press A/D to move continuously, press LEFT/RIGHT to move in discrete jumps,
+and press R to change color (will return to original color when key is released). 
+<br/><br/> 
+This three.js demo is part of a collection at
+<a href="http://stemkoski.github.io/Three.js/">http://stemkoski.github.io/Three.js/</a>
+</div>
+<!-- ------------------------------------------------------------ -->
+
+<div id="ThreeJS" style="position: absolute; left:0px; top:0px"></div>
+<script>
+/*
+	Three.js "tutorials by example"
+	Author: Lee Stemkoski
+	Date: August 2013 (three.js v60)
+ */
+
+// MAIN
+
+// standard global variables
+var container, scene, camera, renderer, controls, stats;
+var clock = new THREE.Clock();
+
+var keyboard = new KeyboardState();
+
+// custom global variables
+var mesh;
+
+init();
+animate();
+
+// FUNCTIONS 		
+function init() 
+{
+	// SCENE
+	scene = new THREE.Scene();
+	// CAMERA
+	var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+	scene.add(camera);
+	camera.position.set(0,150,400);
+	camera.lookAt(scene.position);	
+	// RENDERER
+	if ( Detector.webgl )
+		renderer = new THREE.WebGLRenderer( {antialias:true} );
+	else
+		renderer = new THREE.CanvasRenderer(); 
+	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	container = document.getElementById( 'ThreeJS' );
+	container.appendChild( renderer.domElement );
+	// EVENTS
+	THREEx.WindowResize(renderer, camera);
+	THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
+	// CONTROLS
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	// STATS
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.bottom = '0px';
+	stats.domElement.style.zIndex = 100;
+	container.appendChild( stats.domElement );
+	// LIGHT
+	var light = new THREE.PointLight(0xffffff);
+	light.position.set(100,250,100);
+	scene.add(light);
+	// FLOOR
+	var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
+	floorTexture.repeat.set( 10, 10 );
+	var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	floor.position.y = -0.5;
+	floor.rotation.x = Math.PI / 2;
+	scene.add(floor);
+	// SKYBOX
+	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
+	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+	scene.add(skyBox);
+	
+	////////////
+	// CUSTOM //
+	////////////
+	
+	var geometry = new THREE.SphereGeometry( 30, 32, 16 );
+	var material = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
+	mesh = new THREE.Mesh( geometry, material );
+	mesh.position.set(0,40,0);
+	scene.add(mesh);
+	
+}
+
+function animate() 
+{
+    requestAnimationFrame( animate );
+	render();		
+	update();
+}
+
+function update()
+{
+	keyboard.update();
+
+	var moveDistance = 50 * clock.getDelta(); 
+
+	if ( keyboard.down("left") ) 
+		mesh.translateX( -50 );
+		
+	if ( keyboard.down("right") ) 
+		mesh.translateX(  50 );
+
+	if ( keyboard.pressed("A") )
+		mesh.translateX( -moveDistance );
+		
+	if ( keyboard.pressed("D") )
+		mesh.translateX(  moveDistance );
+		
+	if ( keyboard.down("R") )
+		mesh.material.color = new THREE.Color(0xff0000);
+	if ( keyboard.up("R") )
+		mesh.material.color = new THREE.Color(0x0000ff);
+		
+	controls.update();
+	stats.update();
+}
+
+function render() 
+{
+	renderer.render( scene, camera );
+}
+
+</script>
+
+</body>
+</html>
--- Three.js/js/KeyboardState.js
+++ Three.js/js/KeyboardState.js
@@ -1,124 +1,101 @@
-// THREEx.KeyboardState.js keep the current state of the keyboard.
-// It is possible to query it at any time. No need of an event.
-// This is particularly convenient in loop driven case, like in
-// 3D demos or games.
-//
-// # Usage
-//
-// **Step 1**: Create the object
-//
-// ```var keyboard	= new KeyboardState();```
-//
-// **Step 2**: Query the keyboard state
-//
-// This will return true if shift and A are pressed, false otherwise
-//
-// ```keyboard.pressed("shift+A")```
-//
-// **Step 3**: Stop listening to the keyboard
-//
-// ```keyboard.destroy()```
-//
-// NOTE: this library may be nice as standaline. independant from three.js
-// - rename it keyboardForGame
-//
-// author: Jerome Etienne
-// 
-// # Code
-//
-
 /**
- * - NOTE: it would be quite easy to push event-driven too
- *   - microevent.js for events handling
- *   - in this._onkeyChange, generate a string from the DOM event
- *   - use this as event name
-*/
-KeyboardState	= function()
-{
-	// to store the current state
-	this.keyCodes	= {};
-	this.modifiers	= {};
-	
-	// create callback to bind/unbind keyboard events
-	var self	= this;
-	this._onKeyDown	= function(event){ self._onKeyChange(event, true); };
-	this._onKeyUp	= function(event){ self._onKeyChange(event, false);};
-
+ * @author Lee Stemkoski
+ * August 2013
+ */
+ 
+// initialization
+KeyboardState = function()
+{	
 	// bind keyEvents
-	document.addEventListener("keydown", this._onKeyDown, false);
-	document.addEventListener("keyup", this._onKeyUp, false);
+	document.addEventListener("keydown", KeyboardState.onKeyDown, false);
+	document.addEventListener("keyup",   KeyboardState.onKeyUp,   false);	
 }
 
-/**
- * To stop listening of the keyboard events
-*/
-KeyboardState.prototype.destroy	= function()
+///////////////////////////////////////////////////////////////////////////////
+
+KeyboardState.k = 
+{  
+	8: "backspace", 9: "tab", 		13: "enter",	16: "shift", 
+	17: "ctrl", 	18: "alt",		27: "esc", 		32: "space",
+	33: "pageup", 	34: "pagedown", 35: "end", 		36: "home",
+	37: "left", 	38: "up", 		39: "right", 	40: "down",
+	45: "insert", 	46: "delete",	186: ";", 		187: "=",
+	188: ",", 		189: "-", 		190: ".",		191: "/",
+	219: "[", 		220: "\\", 		221: "]", 		222: "'"
+}
+
+KeyboardState.status = {};
+
+KeyboardState.keyName = function ( keyCode )
 {
-	// unbind keyEvents
-	document.removeEventListener("keydown", this._onKeyDown, false);
-	document.removeEventListener("keyup", this._onKeyUp, false);
+	return ( KeyboardState.k[keyCode] != null ) ? 
+		KeyboardState.k[keyCode] : 
+		String.fromCharCode(keyCode);
 }
 
-KeyboardState.MODIFIERS	= ['shift', 'ctrl', 'alt', 'meta'];
-KeyboardState.ALIAS	= {
-	'left'		: 37,
-	'up'		: 38,
-	'right'		: 39,
-	'down'		: 40,
-	'space'		: 32,
-	'pageup'	: 33,
-	'pagedown'	: 34,
-	'enter'		: 13,
-	'escape'	: 27,
-	'lbracket'	: 219,
-	'rbracket'	: 221,
-	'tab'		: 9
-};
-
-/**
- * to process the keyboard dom event
-*/
-KeyboardState.prototype._onKeyChange	= function(event, pressed)
+KeyboardState.onKeyUp = function(event)
 {
-	// log to debug
-	//console.log("onKeyChange", event, pressed, event.keyCode, event.shiftKey, event.ctrlKey, event.altKey, event.metaKey)
-
-	// update this.keyCodes
-	var keyCode		= event.keyCode;
-	this.keyCodes[keyCode]	= pressed;
-
-	// update this.modifiers
-	this.modifiers['shift']= event.shiftKey;
-	this.modifiers['ctrl']	= event.ctrlKey;
-	this.modifiers['alt']	= event.altKey;
-	this.modifiers['meta']	= event.metaKey;
+	var key = KeyboardState.keyName(event.keyCode);
+	if ( KeyboardState.status[key] )
+		KeyboardState.status[key].pressed = false;
 }
 
-/**
- * query keyboard state to know if a key is pressed of not
- *
- * @param {String} keyDesc the description of the key. format : modifiers+key e.g shift+A
- * @returns {Boolean} true if the key is pressed, false otherwise
-*/
-KeyboardState.prototype.pressed	= function(keyDesc)
+KeyboardState.onKeyDown = function(event)
 {
-	var keys	= keyDesc.split("+");
-	for(var i = 0; i < keys.length; i++){
-		var key		= keys[i];
-		var pressed;
-		if( KeyboardState.MODIFIERS.indexOf( key ) !== -1 )
-		{
-			pressed	= this.modifiers[key];
-		}
-		else if( Object.keys(KeyboardState.ALIAS).indexOf( key ) != -1 )
-		{
-			pressed	= this.keyCodes[ KeyboardState.ALIAS[key] ];
-		}
-		else 
-		{
-			pressed	= this.keyCodes[key.toUpperCase().charCodeAt(0)]
-		}
-		if( !pressed )	return false;
-	};
-	return true;
+	var key = KeyboardState.keyName(event.keyCode);
+	if ( !KeyboardState.status[key] )
+		KeyboardState.status[key] = { down: false, pressed: false, up: false, updatedPreviously: false };
 }
+
+KeyboardState.prototype.update = function()
+{
+	for (var key in KeyboardState.status)
+	{
+		// insure that every keypress has "down" status exactly once
+		if ( !KeyboardState.status[key].updatedPreviously )
+		{
+			KeyboardState.status[key].down        		= true;
+			KeyboardState.status[key].pressed     		= true;
+			KeyboardState.status[key].updatedPreviously = true;
+		}
+		else // updated previously
+		{
+			KeyboardState.status[key].down = false;
+		}
+
+		// key has been flagged as "up" since last update
+		if ( KeyboardState.status[key].up ) 
+		{
+			delete KeyboardState.status[key];
+			continue; // move on to next key
+		}
+		
+		if ( !KeyboardState.status[key].pressed ) // key released
+			KeyboardState.status[key].up = true;
+	}
+}
+
+KeyboardState.prototype.down = function(keyName)
+{
+	return (KeyboardState.status[keyName] && KeyboardState.status[keyName].down);
+}
+
+KeyboardState.prototype.pressed = function(keyName)
+{
+	return (KeyboardState.status[keyName] && KeyboardState.status[keyName].pressed);
+}
+
+KeyboardState.prototype.up = function(keyName)
+{
+	return (KeyboardState.status[keyName] && KeyboardState.status[keyName].up);
+}
+
+KeyboardState.prototype.debug = function()
+{
+	var list = "Keys active: ";
+	for (var arg in KeyboardState.status)
+		list += " " + arg
+	console.log(list);
+}
+
+///////////////////////////////////////////////////////////////////////////////
