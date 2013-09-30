4eb2df3e657798a5c72e22bb16625352ea83a5e8
stemkoski <stemkoski@gmail.com>
2013-09-30 18:46:12 -0400

    Added Tween example.

--- Three.js/js/KeyboardState.js
+++ Three.js/js/KeyboardState.js
@@ -15,13 +15,13 @@ KeyboardState = function()
 
 KeyboardState.k = 
 {  
-	8: "backspace", 9: "tab", 		13: "enter",	16: "shift", 
-	17: "ctrl", 	18: "alt",		27: "esc", 		32: "space",
-	33: "pageup", 	34: "pagedown", 35: "end", 		36: "home",
-	37: "left", 	38: "up", 		39: "right", 	40: "down",
-	45: "insert", 	46: "delete",	186: ";", 		187: "=",
-	188: ",", 		189: "-", 		190: ".",		191: "/",
-	219: "[", 		220: "\\", 		221: "]", 		222: "'"
+    8: "backspace",  9: "tab",       13: "enter",    16: "shift", 
+    17: "ctrl",     18: "alt",       27: "esc",      32: "space",
+    33: "pageup",   34: "pagedown",  35: "end",      36: "home",
+    37: "left",     38: "up",        39: "right",    40: "down",
+    45: "insert",   46: "delete",   186: ";",       187: "=",
+    188: ",",      189: "-",        190: ".",       191: "/",
+    219: "[",      220: "\\",       221: "]",       222: "'"
 }
 
 KeyboardState.status = {};
